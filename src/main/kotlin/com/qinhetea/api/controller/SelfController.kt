96c8662f8ebd8239331372df83999373a7a04f8a
airt <airtial@gmail.com>
2017-10-20 19:13:08 +0800

    fix: get self when not login

--- src/main/kotlin/com/qinhetea/api/controller/SelfController.kt
+++ src/main/kotlin/com/qinhetea/api/controller/SelfController.kt
@@ -1,6 +1,8 @@
 package com.qinhetea.api.controller
 
 import com.qinhetea.api.repository.UserRepository
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +13,9 @@ import java.security.Principal
 class SelfController(val userRepository: UserRepository) {
 
   @GetMapping("")
-  fun get(principal: Principal) = userRepository.findByUsername(principal.name)
+  fun get(principal: Principal?) = when (principal) {
+    null -> ResponseEntity.status(HttpStatus.FORBIDDEN).build<Unit>()
+    else -> ResponseEntity.ok(userRepository.findByUsername(principal.name))
+  }
 
 }
