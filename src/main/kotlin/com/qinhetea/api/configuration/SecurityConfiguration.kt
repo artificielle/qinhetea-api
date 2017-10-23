4472b7a80a69135dff704e320dd929c1546f4e07
muxfe <grzpjdyc1653@126.com>
2017-10-23 19:28:29 +0800

    feat: change web security 302 to ajax response

--- src/main/kotlin/com/qinhetea/api/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/com/qinhetea/api/configuration/SecurityConfiguration.kt
@@ -7,14 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
-import org.springframework.http.MediaType
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
+import javax.servlet.http.HttpServletResponse
 
 @Configuration
 @EnableWebSecurity
@@ -23,21 +24,38 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
 
   override fun configure(http: HttpSecurity) {
     http.
+      exceptionHandling().
+      authenticationEntryPoint { _, response, authException -> authFailHandler(response, authException) }.
+      and().
       authorizeRequests().
       mvcMatchers("/api/users/**").hasRole(User.Role.ADMIN).
       mvcMatchers(HttpMethod.GET, "/api/**").permitAll().
       mvcMatchers("/api/**").authenticated().
       anyRequest().permitAll().and().
       formLogin().
-      successHandler { request, response, authentication ->
-        val user = userRepository.findByUsername(authentication.name).get()
-        response.characterEncoding = "UTF-8"
-        response.writer.write(jacksonObjectMapper().writeValueAsString(user))
-        response.setHeader("Content-Type", "application/json")
-        response.status = 200
-      }.and().
+      failureHandler { _, response, exception -> authFailHandler(response, exception) }.
+      successHandler { _, response, authentication -> loginSuccessHandler(response, authentication) }.
+      and().
+      logout().logoutSuccessHandler { _, response, _ -> logoutHandler(response) }.and().
       csrf().disable().
       httpBasic()
+  }
+
+  fun authFailHandler(response: HttpServletResponse, exception: Exception) {
+    response.status = HttpServletResponse.SC_UNAUTHORIZED
+    response.writer.write(exception.message.orEmpty())
+  }
+
+  fun loginSuccessHandler(response: HttpServletResponse, authentication: Authentication) {
+    response.status = HttpServletResponse.SC_OK
+    response.characterEncoding = "UTF-8"
+    response.setHeader("Content-Type", "application/json")
+    val user = userRepository.findByUsername(authentication.name).get()
+    response.writer.write(jacksonObjectMapper().writeValueAsString(user))
+  }
+
+  fun logoutHandler(response: HttpServletResponse) {
+    response.status = HttpServletResponse.SC_NO_CONTENT
   }
 
   override fun configure(auth: AuthenticationManagerBuilder) {
