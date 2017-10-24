fa38c794c1dbbba291f7922363e8df2c6c37bc76
airt <airtial@gmail.com>
2017-10-24 20:42:55 +0800

    feat: check username conflict

--- src/main/kotlin/com/qinhetea/api/controller/UserController.kt
+++ src/main/kotlin/com/qinhetea/api/controller/UserController.kt
@@ -5,6 +5,7 @@ import com.qinhetea.api.entity.UserRole
 import com.qinhetea.api.repository.UserRepository
 import mu.KotlinLogging
 import org.springframework.data.rest.webmvc.RepositoryRestController
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,16 +19,25 @@ class UserController(override val repository: UserRepository)
 
   @PostMapping("")
   @Secured("ROLE_${User.Role.ADMIN}")
-  fun create(@RequestBody userForm: UserForm) = ResponseEntity.ok(
-    repository.save(userForm.collect().encodePassword())
-  )
+  fun create(@RequestBody userForm: UserForm) =
+    if (repository.findByUsername(userForm.username).isPresent)
+      ResponseEntity.status(HttpStatus.CONFLICT).body(mapOf(
+        "timestamp" to System.currentTimeMillis(),
+        "status" to HttpStatus.CONFLICT.value(),
+        "error" to HttpStatus.CONFLICT.reasonPhrase,
+        "message" to "Conflict username"
+      ))
+    else
+      ResponseEntity.ok(
+        repository.save(userForm.collect().encodePassword())
+      )
 
-  @Suppress("ArrayInDataClass")
+  @Suppress("ArrayInDataClass", "MemberVisibilityCanPrivate")
   data class UserForm(
-    private val nickname: String?,
-    private val username: String,
-    private val password: String,
-    private val roles: Array<UserRole>
+    val nickname: String?,
+    val username: String,
+    val password: String,
+    val roles: Array<UserRole>
   ) {
     fun collect() = User(
       nickname = nickname,
